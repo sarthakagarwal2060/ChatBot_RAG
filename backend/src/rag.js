5d320e9b6a0a5cccbefb959d521aca9188d64ecc
sarthakagarwal2060 <sarthakagarwal6011@gmail.com>
2026-06-24 00:19:13 +0530

    changed chunkoverlap to 200

--- backend/src/rag.js
+++ backend/src/rag.js
@@ -75,8 +75,8 @@ export async function indexDocument(filePath, filename, collectionName) {
 
   const textSplitter = new RecursiveCharacterTextSplitter({
     chunkSize: 1000,
-    // chunkOverlap: 200,
-    chunkOverlap: 190,
+    chunkOverlap: 200,
+    // chunkOverlap: 190,
   });
 
   const docs = await textSplitter.splitDocuments(rawDocs);
